9a0c3d173fbbbc599fbad4311827d4835b254a1c
sergey <mrserjy@gmail.com>
2021-12-27 20:01:31 +0300

    full screen

--- app/src/main/java/com/frose/quizzapp/MainActivity.kt
+++ app/src/main/java/com/frose/quizzapp/MainActivity.kt
@@ -2,10 +2,17 @@ package com.frose.quizzapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
+    @Suppress("DEPRECATION")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        getWindow().setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main)
+
     }
 }
