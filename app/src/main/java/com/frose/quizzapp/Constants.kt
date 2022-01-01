07a74f8f84118203de97de269667ddbff7416fa2
Serg <mrserjy@gmail.com>
2022-01-01 21:46:22 +0300

    add more contnt

--- app/src/main/java/com/frose/quizzapp/Constants.kt
+++ app/src/main/java/com/frose/quizzapp/Constants.kt
@@ -15,6 +15,96 @@ object Constants{
                 1
             )
         )
+        questionList.add(
+            Question(2, "Which country does this flag belong to?",
+                R.drawable.france,
+                "Bahamas",
+                "France",
+                "Armenia",
+                "Austria",
+                2
+            )
+        )
+        questionList.add(
+            Question(3, "Which country does this flag belong to?",
+                R.drawable.israel,
+                "Bahamas",
+                "France",
+                "Armenia",
+                "Israel",
+                4
+            )
+        )
+        questionList.add(
+            Question(4, "Which country does this flag belong to?",
+                R.drawable.italy,
+                "Bahamas",
+                "Italy",
+                "Armenia",
+                "Israel",
+                2
+            )
+        )
+        questionList.add(
+            Question(5, "Which country does this flag belong to?",
+                R.drawable.turkey,
+                "Bahamas",
+                "Italy",
+                "Turkey",
+                "Israel",
+                3
+            )
+        )
+        questionList.add(
+            Question(6, "Which country does this flag belong to?",
+                R.drawable.usa,
+                "USA",
+                "Italy",
+                "Turkey",
+                "Israel",
+                1
+            )
+        )
+        questionList.add(
+            Question(7, "Which country does this flag belong to?",
+                R.drawable.brazil,
+                "USA",
+                "Italy",
+                "Turkey",
+                "Brazil",
+                4
+            )
+        )
+        questionList.add(
+            Question(8, "Which country does this flag belong to?",
+                R.drawable.iceland,
+                "USA",
+                "Iceland",
+                "Turkey",
+                "Israel",
+                2
+            )
+        )
+        questionList.add(
+            Question(9, "Which country does this flag belong to?",
+                R.drawable.russia,
+                "USA",
+                "Italy",
+                "Turkey",
+                "Russia",
+                4
+            )
+        )
+        questionList.add(
+            Question(10, "Which country does this flag belong to?",
+                R.drawable.spain,
+                "Spain",
+                "Italy",
+                "Turkey",
+                "Israel",
+                1
+            )
+        )
         return questionList
     }
 }
