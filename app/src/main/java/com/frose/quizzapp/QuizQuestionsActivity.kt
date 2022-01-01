84c4d6dd2720d74da0164bda4ff44dc8230afa87
Serg <mrserjy@gmail.com>
2022-01-01 17:14:01 +0300

    pupulate first question

--- app/src/main/java/com/frose/quizzapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/frose/quizzapp/QuizQuestionsActivity.kt
@@ -3,6 +3,7 @@ package com.frose.quizzapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_quiz_questions.*
 
 class QuizQuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +12,17 @@ class QuizQuestionsActivity : AppCompatActivity() {
 
         val questionsList = Constants.getQuestions()
         Log.i("Questions Size", "${questionsList.size}")
+
+        var currentPosition = 1
+
+        var currQuestion = questionsList[currentPosition - 1]
+        tvQuestion.text = currQuestion.question
+        questionImage.setImageResource(currQuestion.image)
+        progressBar.setProgress(currentPosition)
+        tvProgress.text = "$currentPosition" + "/" +"${progressBar.max}"
+        textViewQ1.text = currQuestion.optionOne
+        textViewQ2.text = currQuestion.optionTwo
+        textViewQ3.text = currQuestion.optionThree
+        textViewQ4.text = currQuestion.optionFour
     }
 }
